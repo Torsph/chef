426992d36f1f60d9068d522edb625e6066354e51
Thomas Torp <thomas@erupt.no>
2015-01-20 14:10:48 +0100

    More nginx stuff

--- search/attributes/default.rb
+++ search/attributes/default.rb
@@ -6,4 +6,10 @@ default['java']['oracle']['accept_oracle_download_terms'] = 'true'
 default['elasticsearch']['cluster']['name'] = 'pelp-search'
 default['elasticsearch']['discovery']['type'] = 'ec2'
 default['elasticsearch']['discovery']['ec2']['groups'] = 'elasticsearch'
-default['elasticsearch']['cload']['aws']['region'] = 'eu-west'
+default['elasticsearch']['cloud']['aws']['region'] = 'eu-west'
+
+default['elasticsearch']['nginx']['ssl']['cert_file'] = nil
+default['elasticsearch']['nginx']['ssl']['key_file'] = nil
+default['elasticsearch']['nginx']['allow_cluster_api'] = 'true'
+default['elasticsearch']['nginx']['port'] = '80'
+default['elasticsearch']['nginx']['users'] = [{'username' => 'username', 'password' => 'password'}]
